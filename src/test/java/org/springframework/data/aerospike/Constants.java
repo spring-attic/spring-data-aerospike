f40dcce5ed53e085e2fcf3bd72f7abdc79522d87
Peter Milne <helipilot50@gmail.com>
2015-11-03 12:51:15 -0500

    New ec2 instance 54.208.67.159

--- src/test/java/org/springframework/data/aerospike/Constants.java
+++ src/test/java/org/springframework/data/aerospike/Constants.java
@@ -1,7 +1,7 @@
 package org.springframework.data.aerospike;
 
 public class Constants {
-	public static final String AS_CLUSTER = "54.173.156.136";
+	public static final String AS_CLUSTER = "54.208.67.159";
 	public static final int AS_PORT = 3000;
 	public static final int AS_TIMEOUT = 5000;
 
