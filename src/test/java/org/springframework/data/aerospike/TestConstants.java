4e8deab7ac83a96647aea4c295286c51d5331277
Mustafa Zidan <mustafa@zidan.me>
2016-06-15 16:39:51 +0200

    add AS_NAMESPACE constant to point to test name spce
    Signed-off-by: Mustafa Zidan <mustafa@zidan.me>

--- src/test/java/org/springframework/data/aerospike/TestConstants.java
+++ src/test/java/org/springframework/data/aerospike/TestConstants.java
@@ -1,8 +1,8 @@
 package org.springframework.data.aerospike;
 
 public class TestConstants {
-	public static final String AS_CLUSTER = "54.208.67.159";
-	public static final int AS_PORT = 3000;
-	public static final int AS_TIMEOUT = 5000;
-
+    public static final String AS_CLUSTER = "54.208.67.159";
+    public static final String AS_NAMESPACE = "test";
+    public static final int AS_PORT = 3000;
+    public static final int AS_TIMEOUT = 10000;
 }
