a818e98fd5b55e21e73fcda2dd3a3cac1bf38ecb
Mustafa Zidan <mustafa@zidan.me>
2016-06-15 16:21:34 +0200

    migrate to new aerospike helper package
    Signed-off-by: Mustafa Zidan <mustafa@zidan.me>

--- src/main/java/com/aerospike/helper/model/Set.java
+++ src/main/java/com/aerospike/helper/model/Set.java
@@ -20,111 +20,115 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 /**
  * this class represents a Set defined in the cluster
- * 
- * @author peter
  *
+ * @author peter
  */
 public class Set {
-	private Namespace parent;
-	private String name;
-	protected Map<String, NameValuePair> values;
+    private Namespace parent;
+    private String name;
+    protected Map<String, NameValuePair> values;
 
-	public Set(Namespace parent, String info){
-		this.parent = parent;
-		setInfo(info);
-	}
-	public Object getParent() {
-		return this.parent;
-	}
-	@Override
-	public String toString() {
-		return this.name;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		return ((obj instanceof Set) &&
-				(obj.toString().equals(toString())));
-	}
-	public String getName() {
-		return toString();
-	}
+    public Set(Namespace parent, String info) {
+        this.parent = parent;
+        setInfo(info);
+    }
 
-	public void setInfo(String info){
-		//ns_name=test:set_name=demo:n_objects=1:set-stop-write-count=0:set-evict-hwm-count=0:set-enable-xdr=use-default:set-delete=false
-		if (!info.isEmpty()){
-			String[] parts = info.split(":");
-			if (values == null){
-				values = new HashMap<String, NameValuePair>();
-			} 
-			
-			for (String part : parts){
-				String[] kv = part.split("=");
-				String key = kv[0];
-				String value = kv[1];
-				NameValuePair storedValue = values.get(key);
-				if (storedValue == null){
-					storedValue = new NameValuePair(this, key, value);
-					values.put(key, storedValue);
-				} else {
-					storedValue.value = value;
-				}
-			}
-			this.name = (String) values.get("set_name").value;
-		}
-	}
+    public Object getParent() {
+        return this.parent;
+    }
 
-	public void mergeSetInfo(String info){
-		//ns_name=test:set_name=demo:n_objects=1:set-stop-write-count=0:set-evict-hwm-count=0:set-enable-xdr=use-default:set-delete=false
-		if (!info.isEmpty()){
-			String[] parts = info.split(":");
-			if (values == null){
-				values = new HashMap<String, NameValuePair>();
-			} 
-			
-			for (String part : parts){
-				String[] kv = part.split("=");
-				String key = kv[0];
-				String value = kv[1];
-				NameValuePair storedValue = values.get(key);
-				if (storedValue == null){
-					storedValue = new NameValuePair(this, key, value);
-					values.put(key, storedValue);
-				} else {
-					try{
-						Long newValue = Long.parseLong(value);
-						Long oldValue = Long.parseLong(storedValue.value.toString());
-						storedValue.value = Long.toString(oldValue + newValue);
-					} catch (NumberFormatException e){
-						storedValue.value = value;
-					}
-				}
-			}
-			this.name = (String) values.get("set_name").value;
-		}
-	}
-	public void setValues(Map<String, NameValuePair> newValues){
-		this.values = newValues;
-	}
-	
-	public List<NameValuePair> getValues(){
-		List<NameValuePair> result = new ArrayList<NameValuePair>();
-		java.util.Set<String> keys = this.values.keySet();
-		for (String key : keys){
-			NameValuePair nvp = this.values.get(key);
-			result.add(nvp);
-		}
-		return result;
-	}
-	
-	public void clear(){
-		java.util.Set<String> keys = this.values.keySet();
-		for (String key : keys){
-			NameValuePair nvp = this.values.get(key);
-			nvp.clear();
-		}
-	}
-	
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return ((obj instanceof Set) &&
+                (obj.toString().equals(toString())));
+    }
+
+    public String getName() {
+        return toString();
+    }
+
+    public void setInfo(String info) {
+        //ns_name=test:set_name=demo:n_objects=1:set-stop-write-count=0:set-evict-hwm-count=0:set-enable-xdr=use-default:set-delete=false
+        if (!info.isEmpty()) {
+            String[] parts = info.split(":");
+            if (values == null) {
+                values = new HashMap<String, NameValuePair>();
+            }
+
+            for (String part : parts) {
+                String[] kv = part.split("=");
+                String key = kv[0];
+                String value = kv[1];
+                NameValuePair storedValue = values.get(key);
+                if (storedValue == null) {
+                    storedValue = new NameValuePair(this, key, value);
+                    values.put(key, storedValue);
+                } else {
+                    storedValue.value = value;
+                }
+            }
+            this.name = (String) values.get("set_name").value;
+        }
+    }
+
+    public void mergeSetInfo(String info) {
+        //ns_name=test:set_name=demo:n_objects=1:set-stop-write-count=0:set-evict-hwm-count=0:set-enable-xdr=use-default:set-delete=false
+        if (!info.isEmpty()) {
+            String[] parts = info.split(":");
+            if (values == null) {
+                values = new HashMap<String, NameValuePair>();
+            }
+
+            for (String part : parts) {
+                String[] kv = part.split("=");
+                String key = kv[0];
+                String value = kv[1];
+                NameValuePair storedValue = values.get(key);
+                if (storedValue == null) {
+                    storedValue = new NameValuePair(this, key, value);
+                    values.put(key, storedValue);
+                } else {
+                    try {
+                        Long newValue = Long.parseLong(value);
+                        Long oldValue = Long.parseLong(storedValue.value.toString());
+                        storedValue.value = Long.toString(oldValue + newValue);
+                    } catch (NumberFormatException e) {
+                        storedValue.value = value;
+                    }
+                }
+            }
+            this.name = (String) values.get("set_name").value;
+        }
+    }
+
+    public void setValues(Map<String, NameValuePair> newValues) {
+        this.values = newValues;
+    }
+
+    public List<NameValuePair> getValues() {
+        List<NameValuePair> result = new ArrayList<NameValuePair>();
+        java.util.Set<String> keys = this.values.keySet();
+        for (String key : keys) {
+            NameValuePair nvp = this.values.get(key);
+            result.add(nvp);
+        }
+        return result;
+    }
+
+    public void clear() {
+        java.util.Set<String> keys = this.values.keySet();
+        for (String key : keys) {
+            NameValuePair nvp = this.values.get(key);
+            nvp.clear();
+        }
+    }
+
 }
--- src/main/java/com/aerospike/helper/query/TTLQualifier.java
+++ src/main/java/com/aerospike/helper/query/TTLQualifier.java
@@ -16,19 +16,21 @@
 package com.aerospike.helper.query;
 
 import com.aerospike.client.Value;
+
 /**
  * Time to live qualifier - not necessary
- * @author peter
  *
+ * @author peter
  */
 @Deprecated
 public class TTLQualifier extends Qualifier {
-	public TTLQualifier(Value value) {
-		super(QueryEngine.Meta.TTL.toString(), FilterOperation.EQ, value);
-	}
-	@Override
-	protected String luaFieldString(String field) {
-		return "expiry";
-	}
+    public TTLQualifier(Value value) {
+        super(QueryEngine.Meta.TTL.toString(), FilterOperation.EQ, value);
+    }
+
+    @Override
+    protected String luaFieldString(String field) {
+        return "expiry";
+    }
 
 }
